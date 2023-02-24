8c88b06947492ed06ea93cce1f35b0ee9e05f337
YugoOtani <otnhugo0125@gmail.com>
2023-02-24 17:19:14 +0900

    show github profile as json

--- src/GitProf.elm
+++ src/GitProf.elm
@@ -5,7 +5,7 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Http
-import Json.Decode exposing (..)
+import Json.Decode exposing (string)
 import Json.Decode.Pipeline exposing (required)
 
 
@@ -16,25 +16,31 @@ main =
         , view = view
         , subscriptions = \_ -> Sub.none
         }
-type alias Model = {result : String}
+type alias Model = {input : String, result : String}
 type Msg = Search | GotRepo (Result Http.Error String) | Input String
 
 init : () -> (Model, Cmd Msg)
-init _ = ({result = ""}, Cmd.none)
+init _ = ({input = "", result = ""}, Cmd.none)
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model = 
     case msg of 
         Search -> (model, Http.get{
-            url = "http://api.github.com/repos/elm/core"
+            url = "http://api.github.com/users/" ++ model.input
             , expect = Http.expectString GotRepo
             })
+        Input s -> ({model | input = s},Cmd.none)
         GotRepo (Ok repo) -> ({model | result = repo}, Cmd.none)
         GotRepo (Err err) -> ({model | result = Debug.toString err}, Cmd.none)
 
 view : Model -> Html Msg
-view model = div [] 
-    [button [onClick Click] [text "get rep info"]
-    , p [] [text model.result]
+view model = div []
+    [
+    Html.form [onSubmit Search] 
+            [
+                input [value model.input, onInput Input] [],
+                button [disabled (String.length model.input < 1)] [text "Search"] 
+            ]
+    ,p [] [text model.result]
     ]
 
