4d1edbc4bd8e767c174353d69f3aaca1d6afb5cc
YugoOtani <otnhugo0125@gmail.com>
2023-02-27 14:04:48 +0900

    use expectjson

--- src/GitProf.elm
+++ src/GitProf.elm
@@ -16,42 +16,41 @@ main =
         , view = view
         , subscriptions = \_ -> Sub.none
         }
-type alias Model = {input : String, result : String}
-type Msg = Search | GotRepo (Result Http.Error String) | Input String
+type alias Model = {input : String, usr : User}
+type Msg = Search | GotRepo (Result Http.Error User) | Input String
 type alias User = 
     { login : String
     ,avator : String
     ,prof : String
     }   
-
+emptyUser = { login = ""
+            ,avator = ""
+            ,prof   = ""
+            }
 init : () -> (Model, Cmd Msg)
-init _ = ({input = "", result = ""}, Cmd.none)
+init _ = ({input = "", usr = emptyUser}, Cmd.none)
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model = 
     case msg of 
         Search -> (model, Http.get{
             url = "http://api.github.com/users/" ++ model.input
-            , expect = Http.expectString GotRepo --use expectJson
+            , expect = Http.expectJson GotRepo decodeUser
             })
         Input s -> ({model | input = s},Cmd.none)
-        GotRepo (Ok repo) -> ({model | result = repo}, Cmd.none)
-        GotRepo (Err err) -> ({model | result = ""}, Cmd.none)
+        GotRepo (Ok repo) -> ({model | usr = repo}, Cmd.none)
+        GotRepo (Err err) -> ({model | usr = emptyUser}, Cmd.none)
 
 view : Model -> Html Msg
 view model = div [] 
     [Html.form [onSubmit Search] [viewInput model, viewButton model]
-    ,p [] (viewResult model) 
+    ,p [] [text (Debug.toString model.usr)]
     ]
 viewInput model = input [value model.input
                         ,onInput Input
                         ,placeholder "github name"] []
 viewButton model = button [disabled (String.length model.input < 1)] [text "Search"]
-viewResult model = case model.result of
-                    "" -> [text "no such user"]
-                    s  -> case D.decodeString decodeUser s of
-                            Ok(usr) -> [text (Debug.toString usr)]
-                            Err(er) -> [text (Debug.toString er)]
+
 decodeUser : D.Decoder User
 decodeUser = D.succeed User 
         |> P.required "login" D.string
